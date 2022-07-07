1027014431d2d825b6bb73d14477b37f3b6fa5f2
turatbek <temirbekov.tt@gmail.com>
2022-07-07 11:27:40 +0600

    test

---
+++ src/peaksoft/Main.java
@@ -0,0 +1,11 @@
+package peaksoft;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Here we go");
+        System.out.println("Welcome to our homeland");
+    }
+}
+
